043ce9fa662a8eab50e74b230c32a4dfb4a6e2e6
William Grzybowski <wg@FreeBSD.org>
2016-12-28 09:40:07 -0200

    Relative import does not work well with py2

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@
 #
 #####################################################################
 
-from .utils import run_syncdb
+from utils import run_syncdb
 
 if __name__ == '__main__':
     run_syncdb()
