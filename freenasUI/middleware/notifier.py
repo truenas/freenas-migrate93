truenas/freenas-migrate93

commit a7366dfa15276d425825c8c2a6f6995155493053

fix(middleware): Fix TypeError: Only byte strings can be passed to C code

freenasUI/middleware/notifier.py
@@ -57,7 +57,7 @@ GELI_KEY_SLOT = 0
 GELI_RECOVERY_SLOT = 1
 SYSTEMPATH = '/var/db/system'
 PWENC_BLOCK_SIZE = 32
-PWENC_PADDING = '{'
+PWENC_PADDING = b'{'
 PWENC_CHECK = 'Donuts!'
 BACKUP_SOCK = '/var/run/backupd.sock'
 
@@ -660,6 +660,8 @@ class notifier:
         return secret
 
     def pwenc_encrypt(self, text):
+        if not isinstance(text, bytes):
+            text = text.encode('utf8')
         from Crypto.Random import get_random_bytes
         from Crypto.Util import Counter
         pad = lambda x: x + (PWENC_BLOCK_SIZE - len(x) % PWENC_BLOCK_SIZE) * PWENC_PADDING

commit eb4d44e41c4717478e3b87140767a35a355c27fc

fix(middleware): Fix password decoding (broken in a7366dfa15276d425825c8c2a6f6995155493053)

freenasUI/middleware/notifier.py
@@ -687,7 +687,7 @@ class notifier:
             AES.MODE_CTR,
             counter=Counter.new(64, prefix=nonce),
         )
-        return cipher.decrypt(encrypted).decode('utf8').rstrip(PWENC_PADDING)
+        return cipher.decrypt(encrypted).rstrip(PWENC_PADDING).decode('utf8')
 
 
 def usage():